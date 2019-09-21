2899cb42643509403df74fe795f20f217e3faca1
micro <developermicro@outlook.com>
2019-09-21 14:16:10 +0800

    TestNG编写测试“

--- src/main/java/org/testng/EmployeeDetails/EmpBusinessLogic.java
+++ src/main/java/org/testng/EmployeeDetails/EmpBusinessLogic.java
@@ -6,17 +6,18 @@ package org.testng.EmployeeDetails;
 
 public class EmpBusinessLogic {
     // Calculate the yearly salary of employee
-    public double calculateYearlySalary(EmployeeDetails employeeDetails){
-        double yearlySalary=0;
+    public double calculateYearlySalary(EmployeeDetails employeeDetails) {
+        double yearlySalary = 0;
         yearlySalary = employeeDetails.getMonthlySalary() * 12;
         return yearlySalary;
     }
+
     // Calculate the appraisal amount of employee
-    public double calculateAppraisal(EmployeeDetails employeeDetails){
-        double appraisal=0;
-        if(employeeDetails.getMonthlySalary() < 10000){
+    public double calculateAppraisal(EmployeeDetails employeeDetails) {
+        double appraisal = 0;
+        if (employeeDetails.getMonthlySalary() < 10000) {
             appraisal = 500;
-        }else{
+        } else {
             appraisal = 1000;
         }
         return appraisal;
