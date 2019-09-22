3972cdca7aa24ec0b163b6966a5e968308a0ef1c
micro <developermicro@outlook.com>
2019-09-22 16:19:42 +0800

    通过组名给出被依赖的测试方法。

---
+++ src/main/java/org/testng/dependsongroups/Demo.java
@@ -0,0 +1,25 @@
+package org.testng.dependsongroups;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+
+    @Test(dependsOnGroups = "smoke")
+    public void test1(){
+        System.out.println("test1");
+    }
+
+    @Test(groups = {"smoke"})
+    public void test2(){
+        System.out.println("test2");
+    }
+
+    @Test(groups = {"smoke"})
+    public void test3(){
+        System.out.println("test3");
+    }
+}
